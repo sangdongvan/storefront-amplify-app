9e14da6f834b8530d1a7ea5a1218914d8b0196b9
Sang Dong <sang.dongvan@asnet.com.vn>
2024-12-05 11:29:35 +0700

    minor

--- src-rslib/serve.ts
+++ src-rslib/serve.ts
@@ -2,7 +2,7 @@ import express from "express";
 // @ts-ignore
 import compression from "compression";
 
-
+// @ts-ignore
 import { StorefrontApi } from "./config.gen";
 // @ts-ignore
 import expressjsHandler from "./server";
